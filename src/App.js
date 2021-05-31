5d4f9e8a335921b5023d21c056bd1e486150a18d
Ioann <vanes.sysoev@gmail.com>
2021-05-31 16:36:59 +0300

    feat: Modal and Cart
    managing visability of Cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./components/Layout/Header";
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+    const [cartIsShown, setCartIsShown] = useState(false);
+
+    const showCartHadnler = () => {
+        setCartIsShown(true);
+    }
+
+    const hideCartHadnler = () => {
+        setCartIsShown(false);
+    }
+
     return (
         <React.Fragment>
-            <Cart/>
-            <Header />
+            {cartIsShown && <Cart onClose={hideCartHadnler}/>}
+            <Header onShowCart={showCartHadnler} />
             <main>
                 <Meals/>
             </main>
